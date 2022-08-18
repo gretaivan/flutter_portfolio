ff361b3490269a0be4154ad37afd346e4ab59e07
gretaivan <greta.ivan91@gmail.com>
2022-08-18 21:59:46 +0100

    initial commit with one page and widget

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../../widgets/Cards/intro_card.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: IntroCard(),
+        ),
+    );
+  }
+}
