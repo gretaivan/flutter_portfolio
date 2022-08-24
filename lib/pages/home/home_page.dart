bbd84675785aafbda448bad40a47442f4d88c9f9
gretaivan <greta.ivan91@gmail.com>
2022-08-24 18:22:29 +0100

    adds the portfolio header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -7,9 +7,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: IntroCard(text: "Welcome! My name is Greta"),
-        ),
+      body: SafeArea(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Welcome back 👋",
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300)
+                        ),
+                        Text("Greta's portfolio", 
+                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold))
+                      ]
+                    ),
+                  const Icon(
+                    Icons.account_circle,
+                    color: Colors.black, 
+                    size: 30
+                  )
+                ],)
+              ]
+            )
+          )
+        )
+      )
     );
   }
 }
